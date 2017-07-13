f684930bd7722fcdd4a889d331e6fc491831e5f3
y0za <yoza.is12s@gmail.com>
2017-07-13 22:44:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ink-progress-spinner
+> Progress spinner component for [Ink](https://github.com/vadimdemedes/ink)
+
+## License
+MIT License
---
+++ src/index.js
@@ -0,0 +1,112 @@
+/*
+ * Copy spinners from yarnpkg/yarn
+ * https://github.com/yarnpkg/yarn/blob/25890c8cf90ff23c3230ab7b8debe73ef9a80b34/src/reporters/console/spinner-progress.js#L27-L57
+ *
+ * Copyright (c) 2016-present, Yarn Contributors. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+const {h, Text, Component} = require('ink');
+
+const spinners = [
+  '|/-\\',
+  '⠂-–—–-',
+  '◐◓◑◒',
+  '◴◷◶◵',
+  '◰◳◲◱',
+  '▖▘▝▗',
+  '■□▪▫',
+  '▌▀▐▄',
+  '▉▊▋▌▍▎▏▎▍▌▋▊▉',
+  '▁▃▄▅▆▇█▇▆▅▄▃',
+  '←↖↑↗→↘↓↙',
+  '┤┘┴└├┌┬┐',
+  '◢◣◤◥',
+  '.oO°Oo.',
+  '.oO@*',
+  '🌍🌎🌏',
+  '◡◡ ⊙⊙ ◠◠',
+  '☱☲☴',
+  '⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏',
+  '⠋⠙⠚⠞⠖⠦⠴⠲⠳⠓',
+  '⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆',
+  '⠋⠙⠚⠒⠂⠂⠒⠲⠴⠦⠖⠒⠐⠐⠒⠓⠋',
+  '⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠴⠲⠒⠂⠂⠒⠚⠙⠉⠁',
+  '⠈⠉⠋⠓⠒⠐⠐⠒⠖⠦⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈',
+  '⠁⠁⠉⠙⠚⠒⠂⠂⠒⠲⠴⠤⠄⠄⠤⠠⠠⠤⠦⠖⠒⠐⠐⠒⠓⠋⠉⠈⠈',
+  '⢄⢂⢁⡁⡈⡐⡠',
+  '⢹⢺⢼⣸⣇⡧⡗⡏',
+  '⣾⣽⣻⢿⡿⣟⣯⣷',
+  '⠁⠂⠄⡀⢀⠠⠐⠈'
+];
+
+class ProgressSpinner extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      index: 0
+    };
+    this.defaultDelay = 60;
+    this.defaultCharacters = spinners[28].split('');
+    this.intervalID = null;
+  }
+
+  componentDidMount() {
+    const {
+      delay = this.defaultDelay
+    } = this.props;
+
+    this.intervalID = setInterval(() => {
+      const {
+        characters = this.defaultCharacters
+      } = this.props;
+
+      var index = this.state.index + 1;
+      if (index >= characters.length) {
+        index = 0;
+      }
+      this.setState({ index });
+    }, delay);
+  }
+
+  componentWillUnmount() {
+    if (this.intervalID != null) {
+      clearInterval(this.intervalID);
+    }
+  }
+
+  render() {
+    const {
+      characters = this.defaultCharacters
+    } = this.props;
+    const character = characters[this.state.index];
+
+    return h(Text, {}, character);
+  }
+}
+
+module.exports = {
+  ProgressSpinner,
+  spinners
+};
